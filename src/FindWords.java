7d5e065574ec77332cfeaa2535033c63063ebabe
Emmett Jaakkola <56277507+Emmennater@users.noreply.github.com>
2025-05-03 17:11:53 -0500

    Added final state code

--- src/FindWords.java
+++ src/FindWords.java
@@ -1,17 +1,31 @@
 import java.util.ArrayList;
+import java.util.BitSet;
 
 public class FindWords {
     public int[][] dawg;
-    
+    public BitSet finalStates;
+
     /**
      * Constructor for the FindWords class
      * 
      * @param dawg NxM matrix where N is the number of nodes
      *             and M is the number letters in the alphabet
      *             and each entry is the next state.
+     * @param finalStates List of final states
      */
-    FindWords(int[][] dawg) {
+    FindWords(int[][] dawg, BitSet finalStates) {
         this.dawg = dawg;
+        this.finalStates = finalStates;
+    }
+
+    /**
+     * Checks if a given state is a final state
+     *
+     * @param state The state
+     * @return True if the state is a final state and false otherwise
+     */
+    boolean isFinal(int state) {
+        return finalStates.get(state);
     }
 
     /**
